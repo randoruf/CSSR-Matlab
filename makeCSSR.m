a997fb67fc21d622b4da36996d12c4d1aa611df6
雨宮祐華 <masswie@foxmail.com>
2020-04-10 22:47:51 +1000

    Update makeCSSR.m
    Remove unnecessary common flags for g++ compiler.

--- makeCSSR.m
+++ makeCSSR.m
@@ -7,10 +7,6 @@
 %   - gcc 7.4, Ubuntu Linux 
 %   - CLang 11.0, macOS
 %   - Microsoft Visual C++ , MSVC 19.23.28106.4, Windows 
-%_________________________________________________________________________
-% REFERNCE: 
-%   Shalizi, C. R. & Shalizi, K. L. Blind Construction of Optimal Nonlinear
-%    Recursive Predictors for Discrete Sequences. arXiv:cs/0406011 (2004).
 % _________________________________________________________________________
 % Copyright (C) 2020 Haohua Li
 %
@@ -35,29 +31,16 @@ cd(fileparts(which(mfilename)));
 mfile_name       = mfilename('fullpath');
 [cssr_path,~,~]  = fileparts(mfile_name);
 
-% check mex version, and specify C++ standard as C++11/C++14
-cc_opt_prefix = 'COMPFLAGS=''$COMPFLAGS'; 
-myCCompiler = mex.getCompilerConfigurations('C++'); 
-if strcmp(myCCompiler.Manufacturer, 'Microsoft')
-    cc_opt = strcat(cc_opt_prefix, ' /Wall /std:c++14'''); 
-elseif strcmp(myCCompiler.Manufacturer, 'Apple')
-    cc_opt = strcat(cc_opt_prefix, ' -Wall -std:c++11'''); 
-elseif strcmp(myCCompiler.Manufacturer, 'GNU')
-    cc_opt = strcat(cc_opt_prefix, ' -Wall -std:c++11'''); 
-else 
-    error('CSSR: the C/C++ compiler which Matlab is using does not support. Check the compiler version with ''mex -setup C++''')
-end 
-
 % Create objects files (without linking) 
-eval(['mex -c -outdir cpp ' cc_opt ' ' fullfile(cssr_path, 'cpp','Hash.cpp')])
-eval(['mex -c -outdir cpp ' cc_opt ' ' fullfile(cssr_path, 'cpp','States.cpp')])
-eval(['mex -c -outdir cpp ' cc_opt ' ' fullfile(cssr_path, 'cpp','AllStates.cpp')])
-eval(['mex -c -outdir cpp ' cc_opt ' ' fullfile(cssr_path, 'cpp','ParseTree.cpp')])
-eval(['mex -c -outdir cpp ' cc_opt ' ' fullfile(cssr_path, 'cpp','G_Array.cpp')])
-eval(['mex -c -outdir cpp ' cc_opt ' ' fullfile(cssr_path, 'cpp','Hash2.cpp')])
-eval(['mex -c -outdir cpp ' cc_opt ' ' fullfile(cssr_path, 'cpp','Machine.cpp')])
-eval(['mex -c -outdir cpp ' cc_opt ' ' fullfile(cssr_path, 'cpp','TransTable.cpp')])
-eval(['mex -c -outdir cpp ' cc_opt ' ' fullfile(cssr_path, 'cpp','Test.cpp')])
+eval(['mex -c -outdir cpp ' fullfile(cssr_path, 'cpp','Hash.cpp')])
+eval(['mex -c -outdir cpp ' fullfile(cssr_path, 'cpp','States.cpp')])
+eval(['mex -c -outdir cpp ' fullfile(cssr_path, 'cpp','AllStates.cpp')])
+eval(['mex -c -outdir cpp ' fullfile(cssr_path, 'cpp','ParseTree.cpp')])
+eval(['mex -c -outdir cpp ' fullfile(cssr_path, 'cpp','G_Array.cpp')])
+eval(['mex -c -outdir cpp ' fullfile(cssr_path, 'cpp','Hash2.cpp')])
+eval(['mex -c -outdir cpp ' fullfile(cssr_path, 'cpp','Machine.cpp')])
+eval(['mex -c -outdir cpp ' fullfile(cssr_path, 'cpp','TransTable.cpp')])
+eval(['mex -c -outdir cpp ' fullfile(cssr_path, 'cpp','Test.cpp')])
 
 % link object files to a single program : CSSR 
 if ispc 
